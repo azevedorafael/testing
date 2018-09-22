2146666bd9397d5aa3ddf2d7b9de0af76a6c49bc
Rafael <rafaelsilva.fatec@gmail.com>
2018-09-22 16:33:14 -0300

    Change to absolute paths

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../App';
-import CommentBox from '../CommentBox';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 
 it('shows a comment box', () => {
     const wrapped = shallow(<App />);
@@ -9,3 +10,8 @@ it('shows a comment box', () => {
     expect(wrapped.find(CommentBox).length).toEqual(1)
 });
 
+it('shows a comment list', () => {
+    const wrapped = shallow(<App />);
+
+    expect(wrapped.find(CommentList).length).toEqual(1)
+});
