55094314d9511753386ec2a88c0e9e17c6267458
Rafael <rafaelsilva.fatec@gmail.com>
2018-09-22 17:58:05 -0300

    CommentBox component + tests

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,6 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-    return <div>Comment Box</div>;
+class CommentBox extends Component {
+    state = { comment: '' };
+
+    handleChange = event => {
+        this.setState({ comment: event.target.value });
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        // TODO - Call an action creator
+        // TODO - Save the comment
+
+        this.setState({ comment: '' });
+    };
+
+    render() {
+        const { comment } = this.state
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h4>Add a Comment</h4>
+                <textarea
+                    value={comment}
+                    onChange={this.handleChange}
+                />
+                <div>
+                    <button>Submit Comment</button>
+                </div>
+            </form>
+        );
+    }
 };
+
+export default CommentBox;
 
