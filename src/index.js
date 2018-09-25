a489cd42be3d781024b5c790851b877caee90edb
Rafael <rafaelsilva.fatec@gmail.com>
2018-09-24 22:46:43 -0300

    Add Root component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from 'reducers';
-
+import Root from 'Root';
 import App from 'components/App';
 
 ReactDom.render(
-    <Provider store={createStore(
-        reducers,
-        {},
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+    <Root>
         <App />
-    </Provider>,
+    </Root>,
     document.querySelector('#root'));
